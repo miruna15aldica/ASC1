fc20cb4d0bf41a063bf8d5343a903adb7ed4239a
miruna15aldica <miruna15aldica@gmail.com>
2024-04-11 16:59:41 +0300

    Teste de 84p, fara logging, uniteste, graceful shutdown si alte 2 functii GET

---
+++ data_ingestor.py
@@ -0,0 +1,32 @@
+import os
+import json
+import csv
+import pandas as pd
+import os
+# os.cpu_count = numarul maxim de threaduri suportate de OS
+
+class DataIngestor:
+    def __init__(self, csv_path: str):
+        # TODO: Read csv from csv_path
+        data = []
+        with open(csv_path, newline='') as csvfile:
+            dictread = csv.DictReader(csvfile)
+            for i in dictread:
+                data.append(i) # toate datele citite ajung in data
+        self.data = data # acestea sunt stocate in atributul self.data al obiectlui curent
+
+        self.questions_best_is_min = [
+            'Percent of adults aged 18 years and older who have an overweight classification',
+            'Percent of adults aged 18 years and older who have obesity',
+            'Percent of adults who engage in no leisure-time physical activity',
+            'Percent of adults who report consuming fruit less than one time daily',
+            'Percent of adults who report consuming vegetables less than one time daily'
+        ]
+
+        self.questions_best_is_max = [
+            'Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)',
+            'Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic physical activity and engage in muscle-strengthening activities on 2 or more days a week',
+            'Percent of adults who achieve at least 300 minutes a week of moderate-intensity aerobic physical activity or 150 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)',
+            'Percent of adults who engage in muscle-strengthening activities on 2 or more days a week',
+        ]
+
